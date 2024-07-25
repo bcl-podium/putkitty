573ff9f0003d81669315b657a1afa938892d89f0
Barent Langwell <barent@podiumeducation.com>
2024-07-24 17:20:12 -0700

    Update endpoint logic and add image assets

--- index.js
+++ index.js
@@ -4,35 +4,73 @@ const fs = require("fs");
 
 const app = express();
 
-app.get("/:greyscale?/:width/:height", async (req, res) => {
-  let src;
-  if (req.query.image && fs.existsSync(`./img/kitty${req.query.image}.jpg`)) {
-    src = `./img/kitty${req.query.image}.jpg`;
+app.get("/:greyscale(g)?/:width/:height?", async (req, res) => {
+  console.info(req.params);
+  function dimensionsFromParams({ width: widthParam, height: heightParam }) {
+    if (widthParam && !heightParam) {
+      if (!parseInt(widthParam)) {
+        return { width: 400, height: 400 };
+      }
+      return { width: parseInt(widthParam), height: parseInt(widthParam) };
+    }
+
+    if (widthParam && heightParam) {
+      // if width params are bad and height params are good, use height for both
+      // if height params are bad and width params are good, use width for both
+      // if both params are good, parse them and use them
+      if (parseInt(widthParam) === NaN && parseInt(heightParam) === NaN) {
+        return { width: 400, height: 400 };
+      } else if (parseInt(widthParam) === NaN && parseInt(heightParam)) {
+        dimensions.width, (dimensions.length = parseInt(heightParam));
+        return { width: parseInt(heightParam), height: parseInt(heightParam) };
+      } else if (parseInt(heightParam) === NaN && parseInt(widthParam)) {
+        return { width: parseInt(widthParam), height: parseInt(widthParam) };
+      } else {
+        return { width: parseInt(widthParam), height: parseInt(heightParam) };
+      }
+    }
+  }
+
+  async function throwErrorCat(err) {
+    console.error(err);
+    const errorCat = await sharp("./public/crying-cat.jpg")
+      .resize(400, 400)
+      .toFormat("jpg")
+      .toBuffer();
+    res.send(errorCat);
+  }
+
+  // first, obtain the target image or a random image if none specified
+  let src = "";
+  if (
+    req.query.image &&
+    fs.existsSync(`./public/kitty${req.query.image}.jpg`)
+  ) {
+    src = `./public/kitty${req.query.image}.jpg`;
     console.log(src);
   } else {
     // TODO - otherwise grab a random cat - landscape or portrait depending on params
-    src = `./img/kitty1.jpg`;
+    src = `./public/kitty1.jpg`;
   }
 
   try {
     let img;
+    const dimensions = dimensionsFromParams(req.params);
     if (req.params.greyscale === "g") {
       img = await sharp(src)
-        .resize(parseInt(req.params.width), parseInt(req.params.height))
+        .resize(dimensions.width, dimensions.height)
         .greyscale()
         .toFormat("jpg")
         .toBuffer();
     } else {
       img = await sharp(src)
-        .resize(parseInt(req.params.width), parseInt(req.params.height))
+        .resize(dimensions.width, dimensions.height)
         .toFormat("jpg")
         .toBuffer();
     }
     res.send(img);
   } catch (err) {
-    console.error(err);
-    const img = await sharp("./img/crying-cat.jpg").toFormat("jpg").toBuffer();
-    res.send(img);
+    throwErrorCat(err);
   }
 });
 
